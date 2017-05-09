817fb44ff394c95776d5668bf7fb2b125fecf5e6
Andrew Nase <drew.nase@drew.nase-C02P61W5G3QT>
2017-05-09 11:07:11 -0400

    added example usage

--- examples/init.pp
+++ examples/init.pp
@@ -9,4 +9,7 @@
 # Learn more about module testing here:
 # https://docs.puppet.com/guides/tests_smoke.html
 #
-include ::puppet_agent_migrate
+class { '::puppet_agent_migrate':
+  new_master => 'newmaster.mydomain.com',
+  version    => '4.7.0',
+}
